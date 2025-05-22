1dae92d4c4a47d7ca08ed6427623b2718f7b3230
Simon Krainer <ks0376@mci4me.at>
2025-05-22 15:03:38 +0200

    Add function read_data

--- read_data.py
+++ read_data.py
@@ -1,9 +1,22 @@
 import json
 
-# Opening JSON file
-file = open("data/person_db.json")
+def load_person_data():
+    """A Function that knows where the person database is and returns a dictionary with the persons"""
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    return person_data
 
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
+def get_person_list():
+    """A function that returns a list of persons"""
+    person_data = load_person_data()
+    person_list = []
+    for person in person_data:
+        person_list.append(person["lastname"] + " " + person["firstname"])
+    return person_list
 
-person_data
+if __name__ == "__main__":
+    # Test the function
+    person_data = load_person_data()
+    #print(person_data)
+    person_list = get_person_list()
+    print(person_list)
