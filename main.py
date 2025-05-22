2422015835b0ac812826d22da7fdceaedae85d5f
LukasKmci <kl4641@mci4me.at>
2025-05-22 15:18:53 +0200

    change names streamlit

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ st.write("Der Name ist: ", st.session_state.current_user)
 
 # Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
 # Funktionen aufrufen
-person_dict = read_data.load_person_data()
-person_names = read_data.get_person_list(person_dict)
+person_data = read_data.load_person_data()
+person_list = read_data.get_person_list()
 # bzw: wenn Sie nicht zwei separate Funktionen haben
 # person_names = read_data.get_person_list()
 
 # Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
-    options = person_names, key="sbVersuchsperson")
+    options = person_list, key="sbVersuchsperson")
