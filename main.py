cd9782abc8c4947338444a38e6deaf8de123e1e0
LukasKmci <kl4641@mci4me.at>
2025-05-22 16:03:16 +0200

    clean code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import read_data # Ergänzen Ihr eigenes Modul
+import read_data 
 from PIL import Image
 
 st.write("# EKG APP")
@@ -13,12 +13,9 @@ if 'current_user' not in st.session_state:
 st.write("Der Name ist: ", st.session_state.current_user) 
 
 
-# Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre 
-# Funktionen aufrufen
+# Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre Funktionen aufrufen
 person_data = read_data.load_person_data()
 person_list = read_data.get_person_list()
-# bzw: wenn Sie nicht zwei separate Funktionen haben
-# person_names = read_data.get_person_list()
 
 # Nutzen Sie ihre neue Liste anstelle der hard-gecodeten Lösung
 col1, col2 = st.columns(2)
@@ -27,19 +24,15 @@ with col1:
         'Versuchsperson',
         options = person_list, key="sbVersuchsperson")
 
-        # Anlegen des Session State. Bild, wenn es kein Bild gibt
+# Anlegen des Session State. Bild, wenn es kein Bild gibt
 if 'picture_path' not in st.session_state:
         st.session_state.picture_path = 'data/pictures/none.jpg'
 
-    # ...
-
-    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
 if st.session_state.current_user in person_list:
         st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
 
-    # ...
-
-    # Öffne das Bild und Zeige es an
+# Öffne das Bild und Zeige es an
 with col2: 
     image = Image.open(st.session_state.picture_path)
     st.image(image, caption=st.session_state.current_user)
--- read_data.py
+++ read_data.py
@@ -23,24 +23,6 @@ def find_person_data_by_name(name_search):
     # If no person is found, return None
     return None
 
-def picture_display():
-        # Anlegen des Session State. Bild, wenn es kein Bild gibt
-    if 'picture_path' not in st.session_state:
-        st.session_state.picture_path = 'data/pictures/none.jpg'
-
-    # ...
-
-    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
-    if st.session_state.current_user in person_names:
-        st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
-
-    # ...
-
-    # Öffne das Bild und Zeige es an
-    image = Image.open("../" + st.session_state.picture_path)
-    st.image(image, caption=st.session_state.current_user)
-
-    
 
 if __name__ == "__main__":
     # Test the function
