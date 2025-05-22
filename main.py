94c305ca43304304c92e9683451e7f4eac14dcd5
LukasKmci <kl4641@mci4me.at>
2025-05-22 15:45:39 +0200

    Picture path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import read_data # Ergänzen Ihr eigenes Modul
+from PIL import Image
 
 st.write("# EKG APP")
 st.write("## Versuchsperson auswählen")
@@ -23,3 +24,19 @@ person_list = read_data.get_person_list()
 st.session_state.current_user = st.selectbox(
     'Versuchsperson',
     options = person_list, key="sbVersuchsperson")
+
+        # Anlegen des Session State. Bild, wenn es kein Bild gibt
+if 'picture_path' not in st.session_state:
+        st.session_state.picture_path = 'data/pictures/none.jpg'
+
+    # ...
+
+    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+if st.session_state.current_user in person_list:
+        st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+
+    # ...
+
+    # Öffne das Bild und Zeige es an
+image = Image.open(st.session_state.picture_path)
+st.image(image, caption=st.session_state.current_user)
